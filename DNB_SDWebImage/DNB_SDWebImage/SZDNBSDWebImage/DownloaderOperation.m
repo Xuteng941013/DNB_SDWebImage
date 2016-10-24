f862ebc7faf1f79b5898df128e5e5e913b888e2d
徐腾 <xuteng@xutengdeMacBook-Air.local>
2016-10-24 14:51:05 +0800

    完成从网络上随机下载图片

--- DNB_SDWebImage/DNB_SDWebImage/SZDNBSDWebImage/DownloaderOperation.m
+++ DNB_SDWebImage/DNB_SDWebImage/SZDNBSDWebImage/DownloaderOperation.m
@@ -20,16 +20,24 @@
 
 @implementation DownloaderOperation
 
+//1.自定义操作实例化的方法
+//2.此方法先于mian方法执行
 + (instancetype)downloadWithURLStr:(NSString *)URLStr successBlock:(void (^)(UIImage *))successBlock{
+    //1.实例化自定义操作
     DownloaderOperation *op = [[DownloaderOperation alloc] init];
     
+    //2.保存图片地址和下载完成回调,就可以在当前类全局使用,把外界传入的数据变成自己的
     op.URLStr = URLStr;
     op.successBlock = successBlock;
     
+    //3.返回自定义操作
     return op;
 }
 
-
+/**
+ *  重写操作执行的入口方法 : 做你想做的事情,默认就在子线程异步执行的
+    一旦队列调度了操作执行,那么操作就会自动的执行main方法
+ */
 - (void)main{
     
     //下载图片
@@ -42,7 +50,8 @@
     
     //图片下载完成后,通知主线程刷新UI
     [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-        
+        //执行完这行代码,VC里面的successBlock就会执行,并拿到image
+        //注意:在哪个线程回调代码块,那么代码块就在哪个线程执行,代理通知也是这样的
         self.successBlock(image);
     }];
     
--- DNB_SDWebImage/DNB_SDWebImage/ViewController.m
+++ DNB_SDWebImage/DNB_SDWebImage/ViewController.m
@@ -8,6 +8,11 @@
 
 #import "ViewController.h"
 #import "DownloaderOperation.h"
+#import "AFNetworking.h"
+#import "YYModel.h"
+#import "APPModel.h"
+
+
 
 @interface ViewController ()
 
@@ -16,6 +21,12 @@
  */
 @property (strong,nonatomic) NSOperationQueue *queue;
 
+/** 数据源数组 */
+@property (nonatomic,strong) NSArray *appList;
+
+//展示图片
+@property (weak, nonatomic) IBOutlet UIImageView *iconImageView;
+
 @end
 
 @implementation ViewController
@@ -23,21 +34,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //加载数据
+    [self loadJSONData];
+    
     //初始化全局队列
     self.queue = [[NSOperationQueue alloc] init];
     
-    NSString *urlStr = @"http://i9.pdim.gs/096c33ac7af85390fa40c8b2151607d8.jpeg";
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    //生成随机数
+    int randow = arc4random_uniform((u_int32_t)self.appList.count);
+    
+    APPModel *app = self.appList[randow];
     
     //创建自定义操作
-    DownloaderOperation *op = [DownloaderOperation downloadWithURLStr:urlStr successBlock:^(UIImage *image) {
+    DownloaderOperation *op = [DownloaderOperation downloadWithURLStr:app.icon successBlock:^(UIImage *image) {
         
-        NSLog(@"%@---%@",image,[NSThread currentThread]);
+        self.iconImageView.image = image;
+        
     }];
     
     //将自定义操作添加到队列中
     [self.queue addOperation:op];
     
+    
 }
+
+
+
+#pragma mark-获取json数据的主方法
+/*
+ 测试框架是否可行,当获取到appList数据之后,再点击屏幕,实现图片的下载和展示
+ */
+- (void)loadJSONData
+{
+    // 1.创建网络请求管理者
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    // 2.使用网络请求管理者,发送网络请求获取json数据;
+    // GET方法默认是在子线程异步执行的,当AFN获取到网络数据之后,success回调是自动的默认在主线程执行的
+    [manager GET:@"https://raw.githubusercontent.com/zhangxiaochuZXC/SZiOS07_FerverFile/master/apps.json" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        // responseObject就是字典数组 (AFN自动实现字典转模型)
+        NSArray *dictArr = responseObject;
+        // 实现字典转模型 : 字典数组转模型数组
+        self.appList = [NSArray yy_modelArrayWithClass:[APPModel class] json:dictArr];
+        NSLog(@"%@",self.appList);
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@",error);
+    }];
+}
+
+
 
 
 @end
